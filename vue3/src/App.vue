9e7b94576d5b35d92eaeb10e5bf8a72883d8d7f4
egordiets <egordiets@yandex.ru>
2022-10-24 19:07:13 +0300

    hw-9

---
+++ vue3/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div
+      class="my-div"
+      style="border: 1px solid #000; min-height: 50px; margin: 10px 0"
+  ></div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Button @custom-event-1="event1" @custom-event-2="event2"/>
+<!--  <teleport to=".my-div">-->
+<!--    <p>Some text</p>-->
+<!--    <Button @custom-event-1="event1" @custom-event-2="event2"/>-->
+<!--  </teleport>-->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Button from "@/components/Button";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Button
+  },
+  methods: {
+    event1 (data) {
+      console.log(data)
+    },
+    event2 (data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3/src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modal-wrapper">
+    <div class="modal-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal'
+}
+</script>
+
+<style scoped>
+.modal-wrapper {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .90);
+}
+.modal-content {
+  background-color: #fff;
+  width: 80%;
+  height: 300px;
+}
+</style>
